c2e850fb4f6e3ed9038baa3c9885f3817dcc2529
Daniel Rodríguez <daerodriguezme@slabcode.com>
2018-10-04 01:38:08 -0500

    Initial commit

---
+++ Sapiens_book/models/user.js
@@ -0,0 +1,93 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  lastname: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  study_areas: {
+    type: Array,
+    default: []
+  },
+  organization: {
+    type: String,
+    maxlength: 50,
+    default: ""
+  },
+  profile: {
+    type: Object,
+    required: true,
+    default: {}
+  },
+  nacionality: {
+    type: String,
+    default: "Colombiano"
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other', 'Indefinido'],
+    default: 'Indefinido'
+  },
+  languages: {
+    type: Array,
+  },
+  skills: {
+    type: Object
+  },
+  publications: {
+    type: Object,
+    default: {}
+  }
+});
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({
+    _id: this.id,
+    name: this.name
+  }, config.get('jwtPrivateKey'))
+
+  return token
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    lastname: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+    profile: Joi.object().required(),
+    languages: Joi.array().required(),
+    skills: Joi.object().required()
+  }
+
+  return Joi.validate(user, schema)
+}
+
+exports.User = User
+exports.validate = validateUser
