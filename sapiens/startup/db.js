4a8f62c0ffa6388089cbede6e363c5bbb57d828d
Daniel Rodríguez <daerodriguezme@slabcode.com>
2018-11-03 19:14:08 -0500

    Mongo

--- sapiens/startup/db.js
+++ sapiens/startup/db.js
@@ -2,6 +2,6 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/sapiensbook')
+  mongoose.connect('mongodb://mongo/sapiensbook')
     .then(() => winston.info('Connected to MongoDB...'));
 } 
