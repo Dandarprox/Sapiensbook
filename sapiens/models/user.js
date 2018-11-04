426f1715d6716a3ad5ccc0a7929da1518cb7d084
Daniel Rodríguez <daerodriguezme@slabcode.com>
2018-11-03 19:21:12 -0500

    Skills pasa a ser de tipo "String"

--- sapiens/models/user.js
+++ sapiens/models/user.js
@@ -56,7 +56,7 @@ const userSchema = new mongoose.Schema({
     type: Array,
   },
   skills: {
-    type: Object
+    type: String
   },
   publications: {
     type: Object,
@@ -82,7 +82,7 @@ function validateUser(user) {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
     languages: Joi.array().required(),
-    skills: Joi.object().required(),
+    skills: Joi.string().required(),
     organization: Joi.string(),
     study_areas: Joi.array(),
     nationality: Joi.string()
