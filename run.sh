13a28c40a0b52ae283d74631d6fc599945b86d8a
Daniel Rodríguez <daerodriguezme@slabcode.com>
2018-11-03 19:09:52 -0500

    Corrección de script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git pull https://Dandarprox:Veritasium127@github.com/Dandarprox/Sapiensbook.git
+sudo git pull https://Dandarprox:Veritasium127@github.com/Dandarprox/Sapiensbook.git
 sudo docker-compose down
 sudo docker-compose build
 sudo docker-compose up &
