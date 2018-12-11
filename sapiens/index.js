e93a4a660eb4467bc13602e84421c02bc4e8dabd
Santiago Peña <spenas@MacBook-Pro-de-Santiago.local>
2018-12-10 22:11:53 -0500

    .

--- sapiens/index.js
+++ sapiens/index.js
@@ -1,6 +1,10 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const soap = require('soap');
+const http = require('http');
+const { User } = require('./models/user');
+
 
 require('./startup/logging');
 require('./startup/routes')(app);
@@ -8,5 +12,29 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
+var service = {
+    checkUserWService: {
+        checkUserPort: {
+            checkUser : function(args) { 
+              const users = User.findOne({ name: args.name });
+              return {  
+                name : users.name,
+                lastname : users.lastname
+              } 
+              
+            }
+        }
+    }
+  };
+  
+  var xml = require('fs').readFileSync('checkUser.wsdl', 'utf8');
+  
+  var server = http.createServer(function(request,response) {
+      response.end("404: Not Found: "+request.url);
+  });
+  
+  server.listen(8001);
+  soap.listen(server, '/checkUser', service, xml);
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
