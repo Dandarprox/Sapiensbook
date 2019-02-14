4fd68f10c66e304167f1e58867a74bcfaf74f4b7
Daniel Rodriguez <daerodriguezme@slabcode.com>
2019-02-14 10:20:19 -0500

    Updated with sticky-session

--- sapiens/index.js
+++ sapiens/index.js
@@ -42,27 +42,41 @@ var service = {
   
 var xml = require('fs').readFileSync('checkUser.wsdl', 'utf8');
 
-var server = http.createServer(function(request,response) {
-    response.end("404: Not Found: " + request.url);
+// var server = http.createServer(function(request,response) {
+//     response.end("404: Not Found: " + request.url);
+// });
+
+var sticky = require('socketio-sticky-session');
+ 
+sticky(function() {
+  // This code will be executed only in slave workers
+  var http = require('http');
+  var server = http.createServer(function(req, res) {
+    // ....
+  });
+  let io = require('socket.io')(server);
+ 
+  io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket']);
+  io.set('origins', '*:*');
+  
+  io.on('connection', function (socket) {
+    console.log('User connected');
+    socket.on('disconnect', function() {
+      console.log('User disconnected');
+    });
+    socket.on('save-message', function (data) {
+      console.log(data);
+      io.emit('new-message', data);
+    });
+  });
+ 
+  return server;
+}).listen(8002, function() {
+  console.log('server started on 8002 port');
 });
 
-server.listen(8002);
-soap.listen(server, '/checkUser', service, xml);
-
-const io = require('socket.io')(server);
-io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket']);
-io.set('origins', '*:*');
-
-io.on('connection', function (socket) {
-  console.log('User connected');
-  socket.on('disconnect', function() {
-    console.log('User disconnected');
-  });
-  socket.on('save-message', function (data) {
-    console.log(data);
-    io.emit('new-message', data);
-  });
-});
+// server.listen(8002);
+// soap.listen(server, '/checkUser', service, xml);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
